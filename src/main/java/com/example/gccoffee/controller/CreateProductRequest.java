d73095c7301c8b9c130c818b1fea8106160f91ff
pjh612 <pjhjh612@gmail.com>
2022-04-27 21:19:01 +0900

    react 프로젝트를 담기 위해 폴더 구조 변경

--- src/main/java/com/example/gccoffee/controller/CreateProductRequest.java
+++
@@ -1,7 +0,0 @@
-package com.example.gccoffee.controller;
-
-import com.example.gccoffee.model.Category;
-
-public record CreateProductRequest(String name, Category category, long price, String description) {
-
-}
